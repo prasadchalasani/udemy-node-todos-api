7444410008e003c8dd10d3d624b1fdb0d783a75e
Prasad chalasani <prasad9chalasani@gmail.com>
2017-02-21 18:27:30 +0530

    Added Delete to todos

--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e) => {
+    res.status(404).send();
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
